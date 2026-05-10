0b5609c4171f2636d8cb8b1147c2062bf1d7074f
Lin_g <344224934@qq.com>
2026-05-11 03:02:28 +0800

    禁用休眠

--- Sources/MaimaiPOVApp.swift
+++ Sources/MaimaiPOVApp.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 @main
 struct MaimaiPOVApp: App {
+    init() {
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
